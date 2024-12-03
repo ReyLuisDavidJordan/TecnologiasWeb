1039b7effea174b2488c827c6f935833b287ec1a
ReyLuisDavidJordan <luisreyjor12@gmail.com>
2024-12-03 15:51:51 -0500

    validaciones

---
+++ Documents/NetBeansProjects/INTENTO24/src/java/modelo/DepartamentoFacade.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ASUS
+ */
+@Stateless
+public class DepartamentoFacade extends AbstractFacade<Departamento> {
+
+    @PersistenceContext(unitName = "INTENTO24PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public DepartamentoFacade() {
+        super(Departamento.class);
+    }
+    
+}
---
+++ Documents/NetBeansProjects/INTENTO24/src/java/modelo/EmpleadoFacade.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ASUS
+ */
+@Stateless
+public class EmpleadoFacade extends AbstractFacade<Empleado> {
+
+    @PersistenceContext(unitName = "INTENTO24PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EmpleadoFacade() {
+        super(Empleado.class);
+    }
+    
+}
